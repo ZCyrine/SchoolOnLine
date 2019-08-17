8b8192e6442068db4c2bac6c4efcdd70758993f4
ZCyrine <syrine9199@gmail.com>
2019-08-17 21:58:28 +0200

    Updated

--- Routers/TeacherRouter.js
+++ Routers/TeacherRouter.js
@@ -1,4 +1,10 @@
 const teacher = require("../Controllers/TeacherController");
 const router = require("express").Router();
+router.post("/create", teacher.create);
+router.get("/findById/:id", teacher.findById);
+router.get("/findall", teacher.findall);
+router.delete("/delete/:id", teacher.delete);
+router.put("/update/:id", teacher.update);
+router.put("/push/:id", teacher.push);
 
 module.exports = router;
