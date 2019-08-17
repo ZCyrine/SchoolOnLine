3addcbc62f32ae7d5a93017be329d23306baa46f
ZCyrine <syrine9199@gmail.com>
2019-08-17 16:24:41 +0200

    update

--- Routers/StudentRouter.js
+++ Routers/StudentRouter.js
@@ -1,4 +1,8 @@
 const student = require("../Controllers/StudentController");
 const router = require("express").Router();
+router.post("/create", student.create);
+router.delete("/delete/:id", student.delete);
+router.get("/find/:id", student.find);
+router.get("/findall",student.findall);
 
 module.exports = router;
