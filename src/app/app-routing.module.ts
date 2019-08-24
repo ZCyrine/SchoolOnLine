1700bf892ba026d2a73b8fe942aaeddb9609aad0
linda hrichi <hrichilinda@yahoo.fr>
2019-08-24 11:13:12 +0200

    n

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 
 
-<<<<<<< HEAD
-const routes: Routes = [];
-=======
-const routes: Routes = [
 
-];
->>>>>>> f3b15b0753bb41e44b3aefd35f34d2730a810d81
+const routes: Routes = [];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
